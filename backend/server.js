2c507eb87dc8f56f09bbe40663d0e8635cb07010
SHIVANSH KRISHNA <shivanshkrishna2004@gmail.com>
2024-04-30 14:45:34 +0530

    1st part completed -  backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World !!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on Port ${PORT}`);
+});
