4031fb2f97a56ad5c9c411b79d64575d88d19810
SHIVANSH KRISHNA <shivanshkrishna2004@gmail.com>
2024-05-01 05:01:16 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -8,6 +9,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -16,11 +20,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World !!");
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
